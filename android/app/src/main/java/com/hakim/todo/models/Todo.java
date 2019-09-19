7abdac1e03fd6d0fd7fb0c0f04606c14447bb449
hakimks <hakimks@gmail.com>
2019-09-19 15:10:37 +0300

    first commit

---
+++ android/app/src/main/java/com/hakim/todo/models/Todo.java
@@ -0,0 +1,48 @@
+package com.hakim.todo.models;
+
+public class Todo {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Todo(int id, String title, String description, boolean completed ){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+}
